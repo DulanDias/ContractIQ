8d5691c8e9d9c3800f0dadb44ff6dec2ae13cf9d
Dulan Dias <ddias01@qub.ac.uk>
2024-09-12 17:00:40 +0100

    minor updates on landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,      
+    FormsModule,
+    RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
+
 export class LandingComponent {
 
+  constructor(private router: Router) {}
+  
+  // Function to check if the current route matches the given link
+  isActive(route: string): boolean {
+    return this.router.url === `/${route}`;
+  }
 }
