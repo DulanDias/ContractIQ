374fce6eb43e5e8e98d176683eb40b2b60755808
Dulan Dias <ddias01@qub.ac.uk>
2024-09-12 14:11:28 +0100

    Angular material setup

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(FormsModule) // Use FormsModule in standalone app
+    importProvidersFrom(FormsModule), provideAnimationsAsync() // Use FormsModule in standalone app
   ]
 }).catch(err => console.error(err));
